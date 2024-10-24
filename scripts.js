6fb4cffcea98f1cf894f15aad61c2515979e9559
Joshua Wilson-Perkin <josh.t.wp@gmail.com>
2024-10-24 18:24:59 -0400

    Fixes to divide function

--- scripts.js
+++ scripts.js
@@ -10,41 +10,17 @@ screen.textContent = '0';
 
 function assignPreviousString() { previousString = screen.textContent };
 function assignCurrentString() { currentString = screen.textContent };
-function divideNums() { screen.textContent = Number(previousString) / Number(currentString) };
-function multiplyNums() { screen.textContent = Number(previousString) * Number(currentString) };
-function subtractNums() { screen.textContent = Number(previousString) - Number(currentString) };
-function addNums() { screen.textContent = Number(previousString) + Number(currentString) };
-
-function equal() {
-assignCurrentString();
-switch(true) {
-
-	case divideIt:
-	divideNums();
-	divideIt = false;
-	break;
-
-	case multiplyIt:
-	multiplyNums();
-	multiplyIt = false;
-	break;
-
-	case subtractIt:
-	subtractNums();
-	subtractIt = false;
-	break;
-
-	case addIt:
-	addNums();
-	addIt = false;
-	break;
-}
-};
+function divideNums() { screen.textContent = (Number(previousString) / Number(currentString)).toFixed(8) };
+function multiplyNums() { screen.textContent = (Number(previousString) * Number(currentString)).toFixed(8) };
+function subtractNums() { screen.textContent = (Number(previousString) - Number(currentString)).toFixed(8) };
+function addNums() { screen.textContent = (Number(previousString) + Number(currentString)).toFixed(8) };
 
 const clearButton = document.querySelector(".clear");
-clearButton.addEventListener('click', function clearScreen() { screen.textContent = '0';
-previousString = '';
-currentString = ''; });
+clearButton.addEventListener('click', function clearScreen() {
+	screen.textContent = '0';
+	previousString = '';
+	currentString = '';
+});
 
 const plusMinusButton = document.querySelector(".plusMinus");
 plusMinusButton.addEventListener('click', function plusMinus() { screen.textContent = Number(screen.textContent) * -1 });
@@ -53,70 +29,124 @@ const percentageButton = document.querySelector(".percentage");
 percentageButton.addEventListener('click', function percentage() { screen.textContent = Number(screen.textContent) / 100 });
 
 const divideButton = document.querySelector(".divide");
-divideButton.addEventListener('click', function divide() { divideIt = true;
-previousString == '' ? (assignPreviousString(),screen.textContent = '0') : (equal());
+divideButton.addEventListener('click', function divide() {
+	assignPreviousString();
+	screen.textContent = '0';
+	divideIt = true;
 });
 
 const multiplyButton = document.querySelector(".multiply");
-multiplyButton.addEventListener('click', function multiply() { assignPreviousString(); 
-screen.textContent = '0';
-multiplyIt = true; });
+multiplyButton.addEventListener('click', function multiply() {
+	assignPreviousString();
+	screen.textContent = '0';
+	multiplyIt = true;
+});
 
 const subtractButton = document.querySelector(".subtract");
-subtractButton.addEventListener('click', function subtract() { assignPreviousString(); 
-screen.textContent = '0';
-subtractIt = true; });
+subtractButton.addEventListener('click', function subtract() {
+	assignPreviousString();
+	screen.textContent = '0';
+	subtractIt = true;
+});
 
 const addButton = document.querySelector(".add");
-addButton.addEventListener('click', function add() { assignPreviousString();
-screen.textContent = '0';
-addIt = true; });
+addButton.addEventListener('click', function add() {
+	assignPreviousString();
+	screen.textContent = '0';
+	addIt = true;
+});
 
 const equalButton = document.querySelector(".equal");
-equalButton.addEventListener('click', equal());
+equalButton.addEventListener('click', function equal() {
+	assignCurrentString();
+	switch (true) {
+
+		case divideIt:
+			divideNums();
+			divideIt = false;
+			break;
+
+		case multiplyIt:
+			multiplyNums();
+			multiplyIt = false;
+			break;
+
+		case subtractIt:
+			subtractNums();
+			subtractIt = false;
+			break;
+
+		case addIt:
+			addNums();
+			addIt = false;
+			break;
+	}
+});
 
 const nine = document.querySelector(".nine");
-nine.addEventListener('click', function nine() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '9'; });
+nine.addEventListener('click', function nine() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '9';
+});
 
 const eight = document.querySelector(".eight");
-eight.addEventListener('click', function eight() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '8'; });
+eight.addEventListener('click', function eight() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '8';
+});
 
 const seven = document.querySelector(".seven");
-seven.addEventListener('click', function seven() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '7'; });
+seven.addEventListener('click', function seven() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '7';
+});
 
 const six = document.querySelector(".six");
-six.addEventListener('click', function six() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '6'; });
+six.addEventListener('click', function six() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '6';
+});
 
 const five = document.querySelector(".five");
-five.addEventListener('click', function five() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '5'; });
+five.addEventListener('click', function five() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '5';
+});
 
 const four = document.querySelector(".four");
-four.addEventListener('click', function four() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '4'; });
+four.addEventListener('click', function four() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '4';
+});
 
 const three = document.querySelector(".three");
-three.addEventListener('click', function three() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '3'; });
+three.addEventListener('click', function three() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '3';
+});
 
 const two = document.querySelector(".two");
-two.addEventListener('click', function two() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '2'; });
+two.addEventListener('click', function two() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '2';
+});
 
 const one = document.querySelector(".one");
-one.addEventListener('click', function one() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '1'; });
+one.addEventListener('click', function one() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '1';
+});
 
 const zero = document.querySelector(".zero");
-zero.addEventListener('click', function zero() { if (screen.textContent === '0') { screen.textContent = '' };
-screen.textContent += '0'; });
+zero.addEventListener('click', function zero() {
+	if (screen.textContent === '0') { screen.textContent = '' };
+	screen.textContent += '0';
+});
 
 const decimal = document.querySelector(".decimal");
-decimal.addEventListener('click', function decimal() { if (!screen.textContent.includes('.')) { screen.textContent += '.' };
-screen.textContent += '' });
+decimal.addEventListener('click', function decimal() {
+	if (!screen.textContent.includes('.')) { screen.textContent += '.' };
+	screen.textContent += ''
+});
 
 
